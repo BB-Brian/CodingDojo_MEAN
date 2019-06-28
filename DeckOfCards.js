17e80fd541903c22afa4e18b9b0d2c6a82b3e739
Brian Bacungan <bbacungan@gmail.com>
2019-06-27 19:53:47 -0700

    updating deck of cards files with more methods

--- DeckOfCards.js
+++ DeckOfCards.js
@@ -20,8 +20,8 @@ class Card {
 
 
 //test
-// let Unicorn = new Card("Uni", "five", "5");
-// Unicorn.show();
+let Unicorn = new Card("Uni", "five", "5");
+Unicorn.show();
 
 
 // The Deck should contain the 52 standard Cards
@@ -31,63 +31,63 @@ class Card {
 // Deal should return the Card that was dealt and remove it from the Deck
 // Now create a Player class. A Player should have the following functionality:
 
-class Deck {
-    constructor() {
-        this.cards = this.reset();
-        this.shuffle();
-        this.shuffle();
-        this.shuffle();
-        this.shuffle();
-    }
-    reset() {
-        let result = [];
-        ["Hearts", "Spades", "Clubs", "Diamonds"].forEach(suit => {
-            ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"].forEach((card, idx) => {
-                result.push(new Card(suit, card, idx + 1))
-            })
-        })
-        return result;
-    }
-    shuffle() {
-        for(let i = 51; i > 0; i--){
-            let rdx = Math.floor(Math.random() * i - 0 + 1);
-            let temp = this.cards[rdx];
-            this.cards[rdx] = this.cards[i];
-            this.cards[i] = temp;
-        }
-    }
-    deal() {
-        return this.cards.pop();
-    }
-}
+// class Deck {
+//     constructor() {
+//         this.cards = this.reset();
+//         this.shuffle();
+//         this.shuffle();
+//         this.shuffle();
+//         this.shuffle();
+//     }
+//     reset() {
+//         let result = [];
+//         ["Hearts", "Spades", "Clubs", "Diamonds"].forEach(suit => {
+//             ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"].forEach((card, idx) => {
+//                 result.push(new Card(suit, card, idx + 1))
+//             })
+//         })
+//         return result;
+//     }
+//     shuffle() {
+//         for(let i = 51; i > 0; i--){
+//             let rdx = Math.floor(Math.random() * i - 0 + 1);
+//             let temp = this.cards[rdx];
+//             this.cards[rdx] = this.cards[i];
+//             this.cards[i] = temp;
+//         }
+//     }
+//     deal() {
+//         return this.cards.pop();
+//     }
+// }
 
-let myDeck = new Deck();
-// console.log(myDeck);
-// console.log(myDeck.deal());
+// let myDeck = new Deck();
+// // console.log(myDeck);
+// // console.log(myDeck.deal());
 
-class Player {
-    constructor(name, deck = new Deck()) {
-        this.name = name;
-        this.hand = [];
-        let num = 5;
-        while(num -- > 0) {
-            this.draw(deck);
-        } 
-    }
-    draw(deck) {
-        this.hand.push(deck.deal());
-    }
-    discard() {
-        return this.hand.pop();
-    }
-}
+// class Player {
+//     constructor(name, deck = new Deck()) {
+//         this.name = name;
+//         this.hand = [];
+//         let num = 5;
+//         while(num -- > 0) {
+//             this.draw(deck);
+//         } 
+//     }
+//     draw(deck) {
+//         this.hand.push(deck.deal());
+//     }
+//     discard() {
+//         return this.hand.pop();
+//     }
+// }
 
-let Robot = new Player("Robot");
-console.log(Robot);
-console.log(Robot.discard());
+// let Robot = new Player("Robot");
+// console.log(Robot);
+// console.log(Robot.discard());
 
-// The Player should have a name
-// The Player should have a hand (an array of cards taken from a Deck)
-// The Player should be able to take a Card (use the deck.deal method)
-// The Player should be able to discard a Card
+// // The Player should have a name
+// // The Player should have a hand (an array of cards taken from a Deck)
+// // The Player should be able to take a Card (use the deck.deal method)
+// // The Player should be able to discard a Card
 
